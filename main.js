4b0a6b846f27fc3b2be55218c14c04325080d9e1
Timothy Miller <timothymiller@timmer.local>
2014-04-15 01:08:58 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+global.$ = $;
+
+var abar 		    = require('address_bar');
+var folder_view = require('folder_view');
+var path        = require('path');				  // built in node.js module
+var shell       = require('nw.gui').Shell;	// node-webkit
+var numBars     = 1;
+
+$(document).ready(function() {
+	var folder     = new folder_view.Folder($('#files'));
+	var addressbar = new abar.AddressBar($('#addressbar'));
+  var currDir    = process.cwd();
+  var numFolds   = 0;
+  
+  folder.open(currDir);			         // opens current working directory
+  addressbar.set(currDir);
+
+
+  folder.on('navigate', function(dir, mime) {
+    if (mime.type == 'folder') {
+      addressbar.enter(mime);
+    } else {
+      shell.openItem(mime.path);
+    }
+  });
+
+  addressbar.on('navigate', function(dir) {
+    folder.open(dir);
+  });
+
+});
